9bb81e31035a1fb4122ca03f671ac48b3e756ece
Chris <chrisluedtke@gmail.com>
2016-09-29 11:55:48 -0500

    Add files via upload

--- groupme_download.py
+++ groupme_download.py
@@ -3,12 +3,12 @@ import json
 import requests
 import pandas as pd
 
-#Find your personal token at https://dev.groupme.com/docs/v3. In the upper right, click "Access Token". Paste your token in between the '' below. Do not give anybody else your token.
-token = ''
-
 #Go to https://dev.groupme.com/bots/new and choose the group you are interested in. Name the bot and press "Submit". NOTE: this will add the bot to your group.
 #Now on https://dev.groupme.com/bots you will see your newly created bot and your group ID. Paste your group ID between the '' below. You can now delete your bot.
 groupID = ''
+
+#Find your personal token at https://dev.groupme.com/docs/v3. In the upper right, click "Access Token". Paste your token in between the '' below. Do not give anybody else your token.
+token = ''
 
 #This url will now automatically incorporate your group ID and token.
 url = 'https://api.groupme.com/v3/groups/' + groupID + '/messages?token=' + token + '&limit=100'
@@ -29,6 +29,7 @@ def get_all_msgs():
                 last_msg_id = group_msgs[-1]['id']
                 current_msg_count = len(group_msgs)
 
+get_all_msgs()
 df = pd.DataFrame(group_msgs)
 
 #Change the directory to the location you would like your output.csv to be posted to.
